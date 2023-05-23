d3da16eaadadffa368a0a7c93db985dda9daf733
mcast <mcast@ACER-NITRO-MCASTRO>
2023-05-22 22:10:16 -0300

    Será que vale a pena usar CrudRepository?

--- src/main/java/br/com/tdec/webClientEstudo/repository/EmpresaRepository.java
+++ src/main/java/br/com/tdec/webClientEstudo/repository/EmpresaRepository.java
@@ -5,6 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 import br.com.tdec.webClientEstudo.model.empresas.Vertical;
 
 public interface EmpresaRepository extends CrudRepository<Vertical, Long> {
+	/**
+	 * Importante verificar aqui que se usarmos o CrudRepository, vamos ter que ter
+	 * um repository para cada objeto.
+	 * 
+	 * @return
+	 */
 	String check();
 
 	String getEmpresaByCodigo(String codigo);
